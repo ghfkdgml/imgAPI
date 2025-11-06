208f618a94c7440e6fb03af4a650aad2ad9a3190
sh <sh.m3sen@gmail.com>
2025-11-06 17:06:27 +0900

    feat: Initial setup with Spring Boot

---
+++ imgapi/src/main/java/com/project/imgapi/ImgapiApplication.java
@@ -0,0 +1,13 @@
+package com.project.imgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgapiApplication.class, args);
+	}
+
+}
